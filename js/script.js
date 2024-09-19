b06a39eff4dfd6526c7b518ffe6dd6bce6f2d07b
Lou <fuglsangen@live.dk>
2024-09-19 20:36:04 +0200

    Proper grid with temp files.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 mySpan = document.getElementById("image-grid");
+let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
-for (let j = 0; j < 5; j++) {
+for (let j = 0; j < 8; j++) {
     let newSpan = document.createElement('span');
     newSpan.setAttribute("class", "image-row");
-    for (let i = 1; i < 6; i++) {
+    for (let i = 1; i < 9; i++) {
         let newBabby = document.createElement('img');
-        newBabby.setAttribute("src", `images/A${i}.png`)
+        newBabby.setAttribute("src", `images/${alphabet[j]}${i}.png`)
         newSpan.appendChild(newBabby);
     }
     mySpan.appendChild(newSpan);
