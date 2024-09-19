df3ad6f87d4b93738227bc7f7fb1a3bfd8227700
Lou <fuglsangen@live.dk>
2024-09-19 22:12:41 +0200

    click handler thing

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
-mySpan = document.getElementById("image-grid");
+let mySpan = document.getElementById("image-grid");
 let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+const funky = function (id) {
+    let clickedOverlay = document.getElementById(`overlay-${id}`);
+    clickedOverlay.style.visibility = 'visible';
+}
 
 for (let j = 0; j < 8; j++) {
     let newSpan = document.createElement('span');
     newSpan.setAttribute("class", "image-row");
     for (let i = 1; i < 9; i++) {
-        let newBabby = document.createElement('img');
-        newBabby.setAttribute("src", `images/${alphabet[j]}${i}.png`)
-        newSpan.appendChild(newBabby);
+        let id = `${alphabet[j]}${i}`;
+
+        let imgWrapper = document.createElement('span');
+        imgWrapper.setAttribute("class", "map-piece");
+
+        let imageTile = document.createElement('img');
+        imageTile.setAttribute("src", `images/${id}.png`);
+        imageTile.setAttribute("class", "blurred");
+        imageTile.setAttribute("id", id)
+        imageTile.onclick = function () {
+            funky(id);
+        }
+
+        let overlay = document.createElement('div');
+        overlay.setAttribute("class", "overlay");
+        overlay.setAttribute("id", `overlay-${id}`)
+        overlay.style.visibility = 'hidden'
+
+        imgWrapper.appendChild(imageTile);
+        imgWrapper.appendChild(overlay);
+        newSpan.appendChild(imgWrapper);
     }
     mySpan.appendChild(newSpan);
 }
